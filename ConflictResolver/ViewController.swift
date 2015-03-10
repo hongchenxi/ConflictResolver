3543915e9a4d2e7578be667d4ae75ea5fb126268
hongchenxi <hongchenxi86@gmail.com>
2015-03-10 18:32:46 +0800

    Initial Commit

---
+++ ConflictResolver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConflictResolver
+//
+//  Created by 洪晨希 on 15/3/10.
+//  Copyright (c) 2015年 chenxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
